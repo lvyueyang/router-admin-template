dec4b737401fa59ad5f1ae568b803f36610919c0
吕岳阳 <yueyang.lv@ucas.com.cn>
2023-03-02 13:42:45 +0800

    feat(设备选择): 显示 IP

--- src/pages/cluster/module/SelectDevices.tsx
+++ src/pages/cluster/module/SelectDevices.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectProps } from 'antd';
+import { Row, Select, SelectProps, Tag, Tooltip } from 'antd';
 import { useEffect, useState } from 'react';
 import { getDevicesList } from './services';
 import { DeviceItem } from './types';
@@ -16,7 +16,27 @@ export function SelectDevices(props: SelectProps) {
     <Select
       {...props}
       mode="multiple"
-      options={options?.map((item) => ({ value: item.mac_add_str, label: item.host_name }))}
-    />
+      tagRender={(props) => {
+        const current = options.find((item) => item.mac_add_str === props.value);
+        return (
+          <Tooltip title={current?.ip}>
+            <Tag closable onClose={props.onClose}>
+              {current?.host_name || props.value}
+            </Tag>
+          </Tooltip>
+        );
+      }}
+    >
+      {options?.map((item) => {
+        return (
+          <Select.Option value={item.mac_add_str} key={item.mac_add_str} label={item.host_name}>
+            <Row justify="space-between">
+              <b>{item.host_name}</b>
+              <span>{item.ip}</span>
+            </Row>
+          </Select.Option>
+        );
+      })}
+    </Select>
   );
 }
