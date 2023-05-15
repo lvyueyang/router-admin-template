89e94e4f0838a5da5eb918a16609f822b967e93b
吕岳阳 <yueyang.lv@ucas.com.cn>
2023-05-15 13:27:53 +0800

    fix: 支持开启和关闭

--- src/pages/system/loggerCron.tsx
+++ src/pages/system/loggerCron.tsx
@@ -1,19 +1,20 @@
 import Header from '@/components/Header';
 import PageContainer from '@/components/PageContainer';
 import { useRequest } from 'ahooks';
-import { Button, Card, Input, message } from 'antd';
+import { Button, Card, Input, Switch, message } from 'antd';
 import { useEffect, useState } from 'react';
 import { getAlarmThreshold, getLoggerCron, updateLoggerCron } from './module';
 import CronEditor from '@/components/CronEditor';
 
 export default function LoggerCron() {
+  const [open, setOpen] = useState(true);
   const [value, setValue] = useState('0 10-19 * * 1-5');
   const { data, run } = useRequest(() => {
     return getAlarmThreshold().then((res) => res.data.data);
   });
   const { run: submitHandler, loading } = useRequest(
     () => {
-      return updateLoggerCron(value).then(() => {
+      return updateLoggerCron(open ? value : '').then(() => {
         message.success('更新成功');
         run?.();
         return {};
@@ -24,7 +25,11 @@ export default function LoggerCron() {
 
   useEffect(() => {
     getLoggerCron().then((res) => {
-      setValue(res.data.data.value);
+      const value = res.data.data.value;
+      if (value) {
+        setValue(res.data.data.value);
+      }
+      setOpen(!!value);
     });
   }, [data]);
 
@@ -33,11 +38,19 @@ export default function LoggerCron() {
       <Header></Header>
       <PageContainer>
         <Card style={{ maxWidth: 800 }}>
-          <Input value={value} />
-
-          <div style={{ margin: '30px 0' }}>
-            <CronEditor value={value} onChange={setValue} />
+          <div>
+            开启/关闭: <Switch checked={open} onChange={setOpen}></Switch>
           </div>
+          <br />
+          {open && (
+            <>
+              <Input value={value} />
+
+              <div style={{ margin: '30px 0' }}>
+                <CronEditor value={value} onChange={setValue} />
+              </div>
+            </>
+          )}
           <div>
             <Button type="primary" disabled={!value} onClick={submitHandler} loading={loading}>
               保存
